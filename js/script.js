c4ecdc2f6b0ee8847ce578807dac7bd43252bca5
Евгения Гаршина <jane818@yandex.ru>
2021-02-19 08:41:21 +0300

    all

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let num;
+let score;
+
+const question1 = {
+ 	question1: 'вопрос1'
+ }
+
+const question2 = {
+ 	question1: 'вопрос2'
+ }
+
+ const question3 = {
+ 	question1: 'вопрос3'
+ }
+
+ const question4 = {
+ 	question1: 'вопрос4'
+ }
