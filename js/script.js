f1a6f62571173b3e3bbc7cee15cfadcb1df5e1ce
Евгения Гаршина <jane818@yandex.ru>
2021-03-10 19:55:00 +0300

    all

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ let num = '0';
 let score;
 
 const questions = [
-	{ 'question1', answer1: 'a', correctAnswer: 'b', answer3: 'c' }, 
-	{ 'question2', answer1: 'a', answer2: 'b', correctAnswer: 'c' },
-	{ 'question3', correctAnswer: 'a', answer2: 'b', answer3: 'c' },
-	{ 'question4', answer1: 'a', answer2: 'b', correctAnswer: 'c' },
+	{ question: 'question1', answer1: 'a', correctAnswer: 'b', answer3: 'c' }, 
+	{ question: 'question2', answer1: 'a', answer2: 'b', correctAnswer: 'c' },
+	{ question: 'question3', correctAnswer: 'a', answer2: 'b', answer3: 'c' },
+	{ question: 'question4', answer1: 'a', answer2: 'b', correctAnswer: 'c' },
 ] 
 
 const result = questions.filter((item) => {
