a3952258003d2ec263eee0d575b2d85bf8f8c888
Евгения Гаршина <jane818@yandex.ru>
2021-03-13 19:45:23 +0300

    all

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 let num = '0';
 let score;
 
-const questions = [
+const q = [
 	{ question: 'question1', answer1: 'a', correctAnswer: 'b', answer3: 'c' }, 
 	{ question: 'question2', answer1: 'a', answer2: 'b', correctAnswer: 'c' },
 	{ question: 'question3', correctAnswer: 'a', answer2: 'b', answer3: 'c' },
 	{ question: 'question4', answer1: 'a', answer2: 'b', correctAnswer: 'c' },
 ] 
 
-const result = questions.filter((item) => {
-  return item.correctAnswer === 'c';
-})
+function bildQuiz(q){
+	for(let i = 0;i < q.length;i++) {
+		if (i = 'b') {
+			console.log (q[i]);
+		}
+	}
+}
 
-console.log(result);
+bildQuiz(q)
