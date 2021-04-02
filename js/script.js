1dc71f4d59377e23f610cb15c7217499aa1b3934
Евгения Гаршина <jane818@yandex.ru>
2021-04-02 09:40:37 +0300

    all

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
-let num = '0';
-let score;
 
-const q = [
-	{ question: 'question1', answer1: 'a', correctAnswer: 'b', answer3: 'c' }, 
-	{ question: 'question2', answer1: 'a', answer2: 'b', correctAnswer: 'c' },
-	{ question: 'question3', correctAnswer: 'a', answer2: 'b', answer3: 'c' },
-	{ question: 'question4', answer1: 'a', answer2: 'b', correctAnswer: 'c' },
-] 
+let question1 = 'Сколько дней в году?'; 
+let correctAnswer1 = '365';
+let question2 = 'Сколько месяцев в году?';
+let correctAnswer2 = '12';
+let question3 = 'Сколько дней в апреле месяце?';
+let correctAnswer3 = '30';
+let	question4 = 'Сколько  часов в сутках?';
+let correctAnswer4 = '24'; 
 
-const result = q.map((item) => item.correctAnswer) 
-	
-console.log(result);
+let correctResult = 4;
+function userAnswer(question, correctAnswer)
+{
+    let answer = prompt(question);
+    let count = 0;
+    if (answer.toLowerCase() == correctAnswer)
+    {
+        correctResult++;
+    }
+}
+
+userAnswer(question1, correctAnswer1[365]);
+userAnswer(question2, correctAnswer2[12]);
+userAnswer(question3, correctAnswer3[30]);
+userAnswer(question4, correctAnswer4[24]);
